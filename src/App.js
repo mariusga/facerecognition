5a53cfda504495083bd45749266d92a8a41ed610
Marius Galvin <marius.galvin@gmx.de>
2020-10-08 17:56:50 +0200

    building the components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navigation from './components/navigation/navigation'
+import Logo from './components/logo/logo'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <Navigation />
-    {/*<Logo />
-    <ImageLinkForm />
+      <Navigation />
+      <Logo />
+      <ImageLinkForm />
+      {/*
     <FaceRecognition />*/}
     </div>
   );
